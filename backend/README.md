33bbc92a7a4bc887bd4cec7bb6e5f3a93a60ac4c
Soham Patel <soham.patel@student.unsw.edu.au>
2018-10-07 23:49:01 +1100

    Add base files for Backend and Frontend

---
+++ backend/README.md
@@ -0,0 +1,9 @@
+# Backend - Instructions to execute
+1. To install dependencies:
+```bash
+pip install requirements.txt
+```
+2. To run the backend server:
+```bash
+python3 app.py
+```
---
+++ backend/app.py
@@ -0,0 +1,14 @@
+import json
+import requests
+import pandas as pd
+from flask import Flask, request
+from flask_restplus import Resource, Api, fields, inputs, reqparse
+
+app = Flask(__name__)
+api = Api(app, version='1.0', title='NSW fuel prices prediction data service',
+    description='Data service which provide users a prediction of how the fuel prices will change in the future so they are able to make a better judgement on when to refill their fuel tanks.',
+)
+
+if __name__ == '__main__':
+    # run the application
+    app.run(debug=True)
