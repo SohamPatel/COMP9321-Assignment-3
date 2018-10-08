17464d7398c4fca4eca1f0197cfc1f96ac92ba5c
iBungee <ayang98@live.com>
2018-10-09 01:22:32 +1100

    Basic setup for the api

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,18 @@ from flask import Flask, request
 from flask_restplus import Resource, Api, fields, inputs, reqparse
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='NSW fuel prices prediction data service',
-    description='Data service which provide users a prediction of how the fuel prices will change in the future so they are able to make a better judgement on when to refill their fuel tanks.',
+api = Api(app,
+        version='1.0',
+        default="Fuel Price"
+        title='NSW fuel prices prediction data service',
+        description='Data service which provide users a prediction of how the fuel prices will change in the future so they are able to make a better judgement on when to refill their fuel tanks.',
 )
+
+@api.route('/fuelprice/<int:postcode>/<string:fueltype>/<string:brand>')
+class FuelPrice(Resource):
+    @api.doc(description="Gets all predicted prices")
+    def get(self,postocde,fueltype,brand):
+        return None;
 
 if __name__ == '__main__':
     # run the application
