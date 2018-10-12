4a30c643adf4c1c60b0f898f6bdc9967623ebd0c
Soham Patel <soham.patel@student.unsw.edu.au>
2018-10-13 02:22:02 +1100

    Add Predictions page, Fix up GET request

--- frontend/app.py
+++ frontend/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    postcode = request.args.get('postcode')
+    brand = request.args.get('brand')
+    fuel_type = request.args.get('type')
+    print("Postcode:", postcode, "\nBrand:", brand, "\nFuel Type:", fuel_type)
+
+    return render_template('predictions.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
