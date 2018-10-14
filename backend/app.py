239e0ec94721219aab75bb96bf2df30c73d5ec8f
Soham Patel <soham.patel@student.unsw.edu.au>
2018-10-14 16:27:35 +1100

    Create endpoints for getBrands and getFuelTypes

--- backend/app.py
+++ backend/app.py
@@ -12,14 +12,84 @@ api = Api(app,
         description='Data service which provide users a prediction of how the fuel prices will change in the future so they are able to make a better judgement on when to refill their fuel tanks.',
 )
 
+brand_parser = reqparse.RequestParser()
+brand_parser.add_argument('postcode', type=int)
+
+fuel_type_parser = reqparse.RequestParser()
+fuel_type_parser.add_argument('postcode', type=int)
+fuel_type_parser.add_argument('brand', type=str)
+
+drop_list = ['ServiceStationName', 'Address', 'Suburb']
+df = pd.read_csv('test.csv')
+
 @api.route('/fuelprice/<int:postcode>/<string:fueltype>/<string:brand>')
 class FuelPrice(Resource):
     @api.response(404, 'Not found')
     @api.response(200, 'Successful')
-    @api.doc(description="Gets all predicted prices")
+    @api.doc(description="Gets all predicted prices given a postcode, type of fuel and its brand")
     def get(self,postcode,fueltype,brand):
-        return None;
+        return None
+
+@api.route('/getBrands')
+class FuelBrands(Resource):
+    @api.expect(brand_parser, validate=True)
+    @api.response(404, 'Not found')
+    @api.response(200, 'Successful')
+    @api.doc(description="Get all brands for the given postcode")
+    def get(self):
+        args = brand_parser.parse_args()
+        postcode = args.get('postcode')
+
+        brand_df = df.query(f'Postcode == {postcode}')
+
+        if brand_df.empty:
+            # Invalid postcode and/or brand
+            output_response = {
+                "message" : f"No Brands found using the given arguments! Please check if its a valid Postcode."
+            }
+            return output_response, 404
+        
+        else:
+            # Keep brand column only
+            brand_df = brand_df[['Brand']]
+            # Remove duplicate brands
+            brand_df = brand_df.drop_duplicates('Brand')
+
+            brands = list(brand_df['Brand'])
+            return brands, 200
+
+@api.route('/getFuelTypes')
+class FuelTypes(Resource):
+    @api.expect(fuel_type_parser, validate=True)
+    @api.response(404, 'Not found')
+    @api.response(200, 'Successful')
+    @api.doc(description="Get all fuel types for the given postcode and brand")
+    def get(self):
+        args = fuel_type_parser.parse_args()
+        postcode = args.get('postcode')
+        brand = args.get('brand')
+
+        fueltype_df = df.query(f'Postcode == {postcode} and Brand == "{brand}"')
+        print(fueltype_df.empty)
+        if fueltype_df.empty:
+            # Invalid postcode and/or brand
+            output_response = {
+                "message" : f"No Fuel Types found using the given arguments! Please check if its a valid Postcode and Brand."
+            }
+            return output_response, 404
+        
+        else:
+            # Keep fuel type column only
+            fueltype_df = fueltype_df[['FuelCode']]
+            # Remove duplicate fuel types
+            fueltype_df = fueltype_df.drop_duplicates('FuelCode')
+
+            fuel_types = list(fueltype_df['FuelCode'])
+            return fuel_types, 200
 
 if __name__ == '__main__':
+    df = df.drop(columns=drop_list)
+    print (df)
+
     # run the application
     app.run(debug=True)
