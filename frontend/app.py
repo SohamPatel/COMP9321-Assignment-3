166edd0c8ad0a749f7305a30626866e401df405e
iBungee <ayang98@live.com>
2018-10-18 13:15:36 +1100

    Update frontend

--- frontend/app.py
+++ frontend/app.py
@@ -26,14 +26,24 @@ def getFuelPredictions(postcode, brand, fuel):
     args = {"postcode" : postcode, "brand" : brand, "fueltype" : fuel}
     return sendRequest("{}/getFuelPredictions".format(URL), args)
 
-def getCheapestPrice():
-    return 129.54;
+def getCheapest(data):
+    '''
+    Data is a list of dictionary
+    E.g [{'date': '2018/10/19', 'predicted_price': 150.5233789566008},
+         {'date': '2018/10/20', 'predicted_price': 150.53885414912406}]
+    '''
+    minPricedItem = min(data, key=lambda x:x['predicted_price'])
 
-def getCheapestDate():
-    return "17/12/2018";
+    return minPricedItem['date'], round(float(minPricedItem['predicted_price']), 2);
 
-def getPredication():
-    return None;
+def getExpensive(data):
+    maxPricedItem = max(data, key=lambda x:x['predicted_price'])
+    return maxPricedItem['date'], round(float(maxPricedItem['predicted_price']), 2);
+
+def roundPrice(predictions):
+    for i in range(0,len(predictions)):
+        predictions[i]['predicted_price'] = round(float(predictions[i]['predicted_price']),2)
+    return predictions
 
 
 @app.route('/')
@@ -46,7 +56,7 @@ def predict():
     brand = request.args.get('brand')
     fuel_type = request.args.get('type')
     print("Postcode:", postcode, "\nBrand:", brand, "\nFuel Type:", fuel_type)
-
+    '''
     # Use these parameters to perform a GET request to Machine Learning API
     data = requests.get(f'http://127.0.0.1:5100/getFuelPredictions?postcode={postcode}&brand={brand}&fueltype={fuel_type}').json()
     predictions = data['predictions']
@@ -58,6 +68,11 @@ def predict():
         prediction['predicted_price'] = "%.2f" % round(float(prediction['predicted_price']), 2)
         if (prediction['predicted_price'] < cheapest_price):
             cheapest_price = temp_price
+    '''
+    data = getFuelPredictions(postcode, brand, fuel_type)
+    predictions = data['predictions']
+    predictions = roundPrice(predictions)
+    cheapest_date,cheapest_price = getCheapest(predictions)
 
     return render_template('predictions.html', postcode=postcode, brand=brand, fuel_type=fuel_type, cheapest_price=cheapest_price, cheapest_date=cheapest_date, predictions=predictions)
 
