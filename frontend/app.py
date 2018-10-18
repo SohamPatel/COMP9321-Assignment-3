b4a58e30d8e93b45faf27e7b1348ed4896df310e
iBungee <ayang98@live.com>
2018-10-18 11:40:15 +1100

    Frontend chagnes

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request
+import requests
 
 app = Flask(__name__)
 
@@ -15,9 +16,19 @@ def predict():
 
     # Use these parameters to perform a GET request to Machine Learning API
 
-    cheapest_price = 129.54
-    cheapest_date = "17/12/2018"
-    return render_template('predictions.html', postcode=postcode, brand=brand, fuel_type=fuel_type, cheapest_price=cheapest_price, cheapest_date=cheapest_date)
+    #cheapest_price = 129.54
+    #cheapest_date = "17/12/2018"
+    return render_template('predictions.html', postcode=postcode, brand=brand, fuel_type=fuel_type, cheapest_price=getCheapestPrice(), cheapest_date=getCheapestDate())
+
+def getCheapestPrice():
+    return 129.54;
+
+def getCheapestDate():
+    return "17/12/2018";
+
+def getPredication():
+    requests.get()
+    return None;
 
 if __name__ == '__main__':
     app.run(debug=True)
