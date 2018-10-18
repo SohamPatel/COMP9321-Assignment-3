dc06eaa8d4c5c6039813638b1a52c4dd456dac64
Soham Patel <soham.patel@student.unsw.edu.au>
2018-10-18 11:30:44 +1100

    GET prediction prices from backend and display on table

--- frontend/app.py
+++ frontend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for, request
+import requests
 
 app = Flask(__name__)
 
@@ -14,10 +15,18 @@ def predict():
     print("Postcode:", postcode, "\nBrand:", brand, "\nFuel Type:", fuel_type)
 
     # Use these parameters to perform a GET request to Machine Learning API
+    data = requests.get(f'http://127.0.0.1:5100/getFuelPredictions?postcode={postcode}&brand={brand}&fueltype={fuel_type}').json()
+    predictions = data['predictions']
+    
+    cheapest_price = "%.2f" % round(float(predictions[0]['predicted_price']), 2)
+    cheapest_date = predictions[0]['date']
 
-    cheapest_price = 129.54
-    cheapest_date = "17/12/2018"
-    return render_template('predictions.html', postcode=postcode, brand=brand, fuel_type=fuel_type, cheapest_price=cheapest_price, cheapest_date=cheapest_date)
+    for prediction in predictions:
+        prediction['predicted_price'] = "%.2f" % round(float(prediction['predicted_price']), 2)
+        if (prediction['predicted_price'] < cheapest_price):
+            cheapest_price = temp_price
+
+    return render_template('predictions.html', postcode=postcode, brand=brand, fuel_type=fuel_type, cheapest_price=cheapest_price, cheapest_date=cheapest_date, predictions=predictions)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
